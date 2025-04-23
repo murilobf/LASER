abd95578b816886c8f75111b4360321344b06587
murilobf <147562088+murilobf@users.noreply.github.com>
2025-04-23 18:59:57 -0300

    0.5v
    O robô agora desvia de obstáculos, tentando contorná-los

--- codigorobo2ponto0.ino
+++ codigorobo2ponto0.ino
@@ -12,13 +12,25 @@ int duracao;
 int distancia;
 int distancia_min = 35;
 
-void virarDireita(){
+//Função do sensor ultrassonico
+int sonar(){
+  digitalWrite(TRIG, HIGH);
+  delay(1);
+  digitalWrite(TRIG, LOW);
+
+  int dur = pulseIn(ECHO, HIGH);
+  int dist = dur/58.2;
+
+  return dist;
+}
+
+//Funções de movimentação 
+void viraEsquerda(){
   motorE.setSpeed(velocidade);
-  motorQ.setSpeed(velocidade);
+  motorD.setSpeed(velocidade);
 
-  motorE.run(FORWARD);
-  motorQ.run(BACKWARD);
-
+  motorE.run(BACKWARD);
+  motorD.run(FORWARD);
   delay(1000);
 
   motorE.run(RELEASE);
@@ -26,25 +38,41 @@ void virarDireita(){
   delay(1000);
 }
 
-void virarEsquerda(){
+void viraDireita(){
   motorE.setSpeed(velocidade);
-  motorQ.setSpeed(velocidade);
+  motorD.setSpeed(velocidade);
 
-  motorE.run(BACKWARD);
-  motorQ.run(FORWARD);
+  motorE.run(FORWARD);
+  motorD.run(BACKWARD);
+
   delay(1000);
 
   motorE.run(RELEASE);
   motorD.run(RELEASE);
   delay(1000);
+
+  //O código abaixo é responsável por fazer ele voltar pra direção original, se possível
+
+  int dist = sonar();
+
+  //Vira pra direita novamente se necessário
+  if(dist >= 0 && dist <= distancia_min){
+    viraDireita();
+  }
+
+  //Anda um pouco pra ser possível desviar do obstáculo
+  andaFrente();
+
+  //Volta pra direção original
+  viraEsquerda();
 }
 
 void andaFrente(){
   motorE.setSpeed(velocidade);
-  motorQ.setSpeed(velocidade);
+  motorD.setSpeed(velocidade);
 
   motorE.run(FORWARD);
-  motorQ.run(FORWARD);
+  motorD.run(FORWARD);
   delay(1000);
 
   motorE.run(RELEASE);
@@ -57,15 +85,14 @@ void setup() {
   pinMode(ECHO, INPUT);
 }
 
+//Essa variação do código original tenta sempre seguir pelo mesmo caminho,
+//virando pra direita pra desviar de obstáculos mas voltando pra direção original se possível
 void loop() {
 
   //Verifica a distância
-  digitalWrite(TRIG, HIGH);
-  delay(1);
-  digitalWrite(TRIG, LOW);
-  duracao = pulseIn(ECHO, HIGH);
-  distancia = duracao/58.2;
+  distancia = sonar();
 
+  //Tenta contornar o obstáculo (se não for possível, como é o caso de quando atinge a parede, ele começa a dar a volta) 
   if(distancia >= 0 && distancia <= distancia_min){
     viraDireita();
   }
